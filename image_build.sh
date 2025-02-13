617d0ef12b4049a3d8642e3982e188ede876070f
JahCorian Ivery <72903146+Insanityveins@users.noreply.github.com>
2025-02-13 14:39:46 -0500

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -4,4 +4,4 @@
 docker build -t bgtv_bot .
 
 # run image and delete when not running to preserve name
-docker run --name --rm bot_runner bgtv_bot:latest
+docker run --rm --name bot_runner bgtv_bot:latest
